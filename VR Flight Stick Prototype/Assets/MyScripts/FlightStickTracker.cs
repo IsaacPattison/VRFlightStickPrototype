ca80bc1e6ea8e062659ddf90ea8a6981c0f0851f
Isaac Pattison <ipattison1997@gmail.com>
2019-05-13 03:21:10 +1200

    Flight Stick Prototype

---
+++ VR Flight Stick Prototype/Assets/MyScripts/FlightStickTracker.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlightStickTracker : MonoBehaviour
+{
+    // floats for the flightsticks x and z position
+    public float angleX;
+    public float inputX;
+    public float angleZ;
+    public float inputZ;
+
+
+    void Update()
+    {
+        // Use for debugging
+        print(angleX);
+        print(inputX);
+
+        // X Rotation Inputs
+        if (transform.localRotation.eulerAngles.x > 10 && transform.localRotation.eulerAngles.x < 45)
+        {
+            // Creates a value between 0 and 1 to use as an input
+            angleX = transform.localRotation.eulerAngles.x;
+            inputX = angleX / 45;
+        }
+
+        if (transform.localRotation.eulerAngles.x > 315 && transform.localRotation.eulerAngles.x < 350)
+        {
+            // Creates a value between 0 and -1 to use as an input
+            angleX = transform.localRotation.eulerAngles.x;
+            inputX = (angleX -= 360) / 45;
+        }
+
+        if (transform.localRotation.eulerAngles.x < 10 && transform.localRotation.eulerAngles.x > 350)
+        {
+            // sets any angle less than 20 to be set to zero so the flight stick isnt too sensitive
+            angleX = transform.localRotation.eulerAngles.x;
+            inputX = angleX / 0;
+        }
+
+
+        // Z Rotation Inputs
+        if (transform.localRotation.eulerAngles.z > 10 && transform.localRotation.eulerAngles.z < 45)
+        {
+            // Creates a value between 0 and 1 to use as an input
+            angleZ = transform.localRotation.eulerAngles.z;
+            inputZ = angleZ / 45;
+
+        }
+
+        if (transform.localRotation.eulerAngles.z > 315 && transform.localRotation.eulerAngles.z < 350)
+        {
+            // Creates a value between 0 and -1 to use as an input
+            angleZ = transform.localRotation.eulerAngles.z;
+            inputZ = (angleZ -= 360) / 45;
+        }
+
+        if (transform.localRotation.eulerAngles.z < 10 && transform.localRotation.eulerAngles.z > 350)
+        {
+            angleZ = transform.localRotation.eulerAngles.z;
+            inputZ = angleZ / 0;
+        }
+
+    }
+}
---
+++ VR Flight Stick Prototype/Assets/MyScripts/ShipScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipScript : MonoBehaviour
+{
+    [Header("Flight Stick")]
+    public GameObject FlightStick;
+    public FlightStickTracker FlightStickScript;
+
+    public float speed = 90.0f;
+
+    // Reference the flightstick script on play
+    void Start()
+    {
+        FlightStickScript = FlightStick.GetComponent<FlightStickTracker>(); 
+    }
+
+    void Update()
+    {
+        // Get the inputs from the tracker script and use it to angle the plane
+        transform.Rotate(FlightStickScript.inputX, 0.0f, FlightStickScript.inputZ);
+
+        // Make the plane fly forward at a speed
+        transform.position += transform.forward * Time.deltaTime * speed;
+
+        // Decrease the speed of the plane when flying upwards
+        speed -= transform.forward.y * Time.deltaTime * 50.0f;
+
+        if (speed < 35.0f)
+        {
+            speed = 35.0f;
+        }
+
+        // If our position is lower than the terrain then update the plane position
+        // to be at the terrain height
+        float terrainHeightPlaneLocation = Terrain.activeTerrain.SampleHeight(transform.position);
+
+        if (terrainHeightPlaneLocation > transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, terrainHeightPlaneLocation, transform.position.z);
+        }
+
+
+
+        
+    }
+}
